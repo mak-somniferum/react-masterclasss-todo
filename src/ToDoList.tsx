2afc3dd18512f0d8f401f4aa80815f10070e7457
mak-somniferum <suhjin700@gmail.com>
2023-06-20 15:55:40 +0900

    Todo - use react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,28 +1,47 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList(){
-  const [todo, setTodo] = useState("");
-  const [todoError, setTodoError] = useState("");
-  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-    const { 
-      currentTarget: { value }, 
-    } = event;
-    setTodoError("");
-    setTodo(value);
-  }
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(todo)
-    if (todo.length < 10) {
-      return setTodoError("To do should be longer");
-    }
-  }
+// function ToDoList(){
+//   const [todo, setTodo] = useState("");
+//   const [todoError, setTodoError] = useState("");
+//   const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+//     const { 
+//       currentTarget: { value }, 
+//     } = event;
+//     setTodoError("");
+//     setTodo(value);
+//   }
+//   const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(todo)
+//     if (todo.length < 10) {
+//       return setTodoError("To do should be longer");
+//     }
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={todo} placeholder="write a to do" />
+//         <button>Add</button>
+//         {todoError !== "" ? todoError : null}
+//       </form>
+//     </div>
+//   );
+// }
+
+function ToDoList (){
+  const { register, watch } = useForm();
+  // console.log(register("todo"))
+  console.log(watch())
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder="write a to do" />
+      <form>
+        <input {...register("Email")} placeholder="Email" />
+        <input {...register("First Name")} placeholder="First Name" />
+        <input {...register("Last Name")} placeholder="Last Name" />
+        <input {...register("Phone Number")} placeholder="Phone Number" />
+        <input {...register("Address")} placeholder="Address" />
         <button>Add</button>
-        {todoError !== "" ? todoError : null}
       </form>
     </div>
   );
