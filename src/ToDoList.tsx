5b8c20453294808b6b9f5e690151f7ea31e58acd
mak-somniferum <suhjin700@gmail.com>
2023-06-20 15:18:39 +0900

    Todo

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,31 @@
-function ToDoList(){
+import { useState } from "react";
 
-  return (<>hello</>);
+function ToDoList(){
+  const [todo, setTodo] = useState("");
+  const [todoError, setTodoError] = useState("");
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+    const { 
+      currentTarget: { value }, 
+    } = event;
+    setTodoError("");
+    setTodo(value);
+  }
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(todo)
+    if (todo.length < 10) {
+      return setTodoError("To do should be longer");
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={todo} placeholder="write a to do" />
+        <button>Add</button>
+        {todoError !== "" ? todoError : null}
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
