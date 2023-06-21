92adfa54025106d1260ac3c3baba0cd34d85289e
mak-somniferum <suhjin700@gmail.com>
2023-06-21 10:39:21 +0900

    Todo - add recoil

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,15 +1,25 @@
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
+
+const toDoState = atom({
+  key: "toDo",
+  default: [],
+});
 
 interface IForm {
   toDo: string;
 }
+
 function ToDoList() {
+  const [value, modFn] = useRecoilState(toDoState);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     setValue,
   } = useForm<IForm>();
+
   const onSubmit = (data: IForm) => {
     console.log("add to do", data.toDo);
     setValue("toDo", "");
@@ -17,6 +27,7 @@ function ToDoList() {
 
   return (
     <div>
+      <h1>To dos</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           {...register("toDo", {
@@ -27,6 +38,9 @@ function ToDoList() {
         <span>{errors?.toDo?.message}</span>
         <button>Add</button>
       </form>
+      <ul>
+        <li></li>
+      </ul>
     </div>
   );
 }
