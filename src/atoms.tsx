30dda68124d46184a7d0595b92d6a00ef6fb89cf
mak-somniferum <suhjin700@gmail.com>
2023-07-10 17:44:47 +0900

    Todo - use recoil selector

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,7 +1,15 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { IToDo } from "./interface";
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [toDos.filter(toDo => toDo.category === "TO_DO"), toDos.filter(toDo => toDo.category === "DOING"), toDos.filter(toDo => toDo.category === "DONE")];
+  },
 });
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,17 +1,33 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector); // selector가 배열을 return 할 때는 좌변처럼 변수가 아닌 배열로 표현할 수 있다.
   return (
     <div>
       <h1>To dos</h1>
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map(toDo => (
+        {toDo.map(toDo => (
           <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map(doing => (
+          <ToDo key={doing.id} {...doing} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map(done => (
+          <ToDo key={done.id} {...done} />
         ))}
       </ul>
     </div>
