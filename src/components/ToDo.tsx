b15e806a0163b0638e757411f5007e2c5aabe1bf
mak-somniferum <suhjin700@gmail.com>
2023-07-10 15:12:59 +0900

    Todo - add button & click event

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,7 +1,34 @@
 import { IToDo } from "../interface";
+import { toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
-  return <li>{text}</li>;
+function ToDo({ text, category, id }: IToDo) {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    console.log("I wanna to " + name);
+  };
+
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
 }
 
 export default ToDo;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme } from './theme';
-import { RecoilRoot } from 'recoil';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme";
+import { RecoilRoot } from "recoil";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
-        <App />
-      </ThemeProvider>
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
 );
