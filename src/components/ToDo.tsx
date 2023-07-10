b1891bd73d28e2a1062ed11cb54c262897128d55
mak-somniferum <suhjin700@gmail.com>
2023-07-10 16:45:23 +0900

    Todo - modified click event with useSetRecoilState

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,29 +1,32 @@
+import { useSetRecoilState } from "recoil";
 import { IToDo } from "../interface";
 import { toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const {
-      currentTarget: { name },
-    } = event;
-    console.log("I wanna to " + name);
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory: IToDo["category"]) => {
+    setToDos(oldToDos => {
+      const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+      const newToDo = { id, text, category: newCategory };
+      return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex + 1)];
+    });
   };
 
   return (
     <li>
       <span>{text}</span>
       {category !== "DOING" && (
-        <button name="DOING" onClick={onClick}>
+        <button name="DOING" onClick={() => onClick("DOING")}>
           Doing
         </button>
       )}
       {category !== "TO_DO" && (
-        <button name="TO_DO" onClick={onClick}>
+        <button name="TO_DO" onClick={() => onClick("TO_DO")}>
           To Do
         </button>
       )}
       {category !== "DONE" && (
-        <button name="DONE" onClick={onClick}>
+        <button name="DONE" onClick={() => onClick("DONE")}>
           Done
         </button>
       )}
